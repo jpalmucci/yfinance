f0abd503919b21955f25c70bab9898cb50365af6
Gregory Burd <gburd@Macintosh.local>
2010-04-09 23:10:54 -0400

    Support for uploading to Clojars.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject yfinance "0.01"
+(defproject org.clojars.gregburd/yfinance "0.1.0"
   :description "Clojure code to access Yahoo! Finance"
   :author "Baishampayan Ghose"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clj-time "0.1.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]])
-
-
+  :dev-dependencies [[swank-clojure "1.1.0"]
+		     [lein-clojars "0.5.0"]]
+  :repositories [["clojars" "http://clojars.org/repo"]])
